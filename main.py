eea62e850df95431d7f72f5c8722a503d2ff6251
Oryna Zudilova <zarinamcnulty@gmail.com>
2025-04-21 17:27:58 +0300

    Add sample data files

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from app.io.input import get_input
-from app.io.output import show_output
+# main.py
+
+from app.io.input import get_input_text, read_file_builtin, read_file_pandas
+from app.io.output import print_to_console, write_to_file
 
 def main():
-    name = get_input()
-    show_output(name)
+    text1 = get_input_text()
+    text2 = read_file_builtin()
+    text3 = read_file_pandas()
+
+    print_to_console(text1)
+    print_to_console(text2)
+    print_to_console(str(text3))
+
+    write_to_file(text1)
+    write_to_file(text2)
+    write_to_file(str(text3))
 
 if __name__ == "__main__":
     main()
